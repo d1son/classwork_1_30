27ae9ba7cae6f87647aa376eb083ae631df63479
d1son <dson120@gmail.com>
2016-01-30 13:49:46 -0500

    added more classwork, add, delete from the prompt

--- index.js
+++ index.js
@@ -8,13 +8,48 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT name, favorite_food, personality FROM happy_bears;', function(err, rows, fields) {
-  if (err) throw err;
+// connection.query('SELECT name, favorite_food, personality FROM happy_bears;', function(err, rows, fields) {
+//   if (err) throw err;
 
-  for (var i = 0; i < rows.length; i++){
-  	console.log("My name is " + rows[i].name + " , I love to eat " + rows[i].favorite_food + " and I am " + rows[i].personality + " all the time!" );
-  }
+//   for (var i = 0; i < rows.length; i++){
+//   	console.log("My name is " + rows[i].name + " , I love to eat " + rows[i].favorite_food + " and I am " + rows[i].personality + " all the time!" );
+//   }
  
-});
+// });
+
+var showBears = function(){
+	connection.query("SELECT * FROM happy_bears;", function(err, rows, fields) {
+		if(err) throw err;
+
+		console.log(rows);
+	});
+};
+
+var addBearName = function(){
+	connection.query("INSERT INTO happy_bears (name, favorite_food, personality) VALUES (?,?,?)", ["happy", "burritos", "mad"], function(err, results) {
+		if (err) throw err;
+
+		console.log("Insert finished!!");
+
+	})
+};
+
+var deleteBear = function(){
+	var query = "DELETE FROM happy_bears WHERE id=?";
+	var idToBeDeleted = process.argv[2]; //user input, user can directly change database from command line
+
+
+	connection.query(query, idToBeDeleted, function(err, results){
+		if(err){
+			throw err;
+		}
+		console.log("Bear " + idToBeDeleted + " was deleted.")
+	});
+}
+
+deleteBear();
+// addBearName();
+showBears();
  
+
 connection.end();
